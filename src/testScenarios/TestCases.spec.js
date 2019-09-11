aa315000ca3188f99732f48d5ed3f5e69846ab31
Tanvir Ahmed <tanvir.ahmed3016@gmail.com>
2019-09-11 13:28:41 +0800

    Update TestCases.spec.js

--- src/testScenarios/TestCases.spec.js
+++ src/testScenarios/TestCases.spec.js
@@ -3,26 +3,26 @@ import tourPage from '../PageObjectModel/ToursPage';
 import currencyPage from '../PageObjectModel/CurrencyPage'
 const expect = require('chai').expect;
 
-// describe('Currencies functionality:', () => {
-//     it('- should show the correct page title and page url.', () => {
-//         loginPage.loginWithCredentials("admin@phptravels.com","demoadmin");
-//         const menuBar = $('#social-sidebar-menu');
-//         menuBar.waitForExist(5000);
-//         currencyPage.goTocurrencyPage();
-//         expect(browser.getUrl()).to.contains('https://www.phptravels.net/admin/settings/currencies/');
-//         expect(currencyPage.currencyPageTitle.getText()).to.equal('CURRENCIES MANAGEMENT')
-//     })
-//     it('- should show error message if currency creation have no data',()=>{
-//         currencyPage.addNewCurrencyWithNoData();
-//         expect(currencyPage.errorMessageforBlankData.getText()).to.equal('Some fields are likely to contain errors. Fix errors and try again.');
-//         browser.pause(10000);
-//         currencyPage.returnButton.click();
-//     })
-//     it('- should able to create new currency from add currency',()=>{
-//         currencyPage.addNewCurrency.waitForVisible(5000);
-//         currencyPage.addNewCurrencies();
-//     })
-// })
+describe('Currencies functionality:', () => {
+    it('- should show the correct page title and page url.', () => {
+        loginPage.loginWithCredentials("admin@phptravels.com","demoadmin");
+        const menuBar = $('#social-sidebar-menu');
+        menuBar.waitForExist(5000);
+        currencyPage.goTocurrencyPage();
+        expect(browser.getUrl()).to.contains('https://www.phptravels.net/admin/settings/currencies/');
+        expect(currencyPage.currencyPageTitle.getText()).to.equal('CURRENCIES MANAGEMENT')
+    })
+    it('- should show error message if currency creation have no data',()=>{
+        currencyPage.addNewCurrencyWithNoData();
+        expect(currencyPage.errorMessageforBlankData.getText()).to.equal('Some fields are likely to contain errors. Fix errors and try again.');
+        browser.pause(10000);
+        currencyPage.returnButton.click();
+    })
+    it('- should able to create new currency from add currency',()=>{
+        currencyPage.addNewCurrency.waitForVisible(5000);
+        currencyPage.addNewCurrencies();
+    })
+})
 
 describe('Tours functionality:',()=>{
     it('- should show the correct page title and page url.',()=>{
@@ -33,9 +33,6 @@ describe('Tours functionality:',()=>{
         const getToursPageTitle = tourPage.toursPageTitle.getText();
         expect(getToursPageTitle).to.equal('TOURS MANAGEMENT');
         expect(browser.getUrl()).to.contains('https://www.phptravels.net/admin/tours');
-    })
-    it('- should show error message for blank add new tours creation',()=>{
-
     })
 })
 
